4bd4ac511d72253c8e89397e6b780ae90ecc4f92
Paulo Bueno <paulob.bruno@gmail.com>
2019-01-20 23:42:05 -0200

    execution tests

--- libpythonpro/spam/spam_sender.py
+++ libpythonpro/spam/spam_sender.py
@@ -1,3 +1,9 @@
 class Sender:
     def send(self, sender, receiver, subject, content):
+        if '@' not in sender:
+            raise InvalidEmail(f'Invalid sender {sender}')
         return sender
+
+
+class InvalidEmail(Exception):
+    pass
--- libpythonpro/tests/test_spam/test_spam_sender.py
+++ libpythonpro/tests/test_spam/test_spam_sender.py
@@ -1,5 +1,5 @@
 import pytest
-from libpythonpro.spam.spam_sender import Sender
+from libpythonpro.spam.spam_sender import Sender, InvalidEmail
 
 
 def test_create_spam_sender():
@@ -9,7 +9,7 @@ def test_create_spam_sender():
 
 @pytest.mark.parametrize(
             'sender',
-            ['paulob.bruno@gmail.com','paulo.siqueira@amaro.com']
+            ['paulob.bruno@gmail.com', 'paulo.siqueira@amaro.com']
             )
 def test_sender(sender):
     email_sender = Sender()
@@ -20,3 +20,18 @@ def test_sender(sender):
         'Os itens a serem comprados'
     )
     assert sender in result
+
+
+@pytest.mark.parametrize(
+            'sender',
+            [' ', 'paulo.siqueiraamaro.com']
+            )
+def test_invalidemail(sender):
+    email_sender = Sender()
+    with pytest.raises(InvalidEmail):
+        email_sender.send(
+            sender,
+            'thaism.nishimoto@gmail.com',
+            'Lista de compras',
+            'Os itens a serem comprados'
+        )
