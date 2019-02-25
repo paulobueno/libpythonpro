d8dc101c033e761b0d01f2809f6a95b13c786fab
Paulo Bueno <paulob.bruno@gmail.com>
2019-02-24 23:12:23 -0300

    dependecy-injection

--- libpythonpro/spam/spam_sender.py
+++ libpythonpro/spam/spam_sender.py
@@ -1,11 +1,8 @@
 class Sender:
-    def __init__(self):
-        self.qty_sent_emails = 0
 
     def send(self, sender, receiver, subject, content):
         if '@' not in sender:
             raise InvalidEmail(f'Invalid sender {sender}')
-        self.qty_sent_emails += 1
         return sender
 
 
--- libpythonpro/tests/test_spam/test_user_dw_send.py
+++ libpythonpro/tests/test_spam/test_user_dw_send.py
@@ -4,6 +4,18 @@ from libpythonpro.spam.models import User
 from libpythonpro.spam.spam_sender import Sender
 
 
+class SenderMock(Sender):
+
+    def __init__(self):
+        super().__init__()
+        self.sending_parameters = None
+        self.qty_sent_emails = 0
+
+    def send(self, sender, receiver, subject, content):
+        self.sending_parameters = (sender, receiver, subject, content)
+        self.qty_sent_emails += 1
+
+
 @pytest.mark.parametrize(
     'users', [
         [
@@ -18,7 +30,7 @@ from libpythonpro.spam.spam_sender import Sender
 def test_qty_spam_sender(session, users):
     for user in users:
         session.save(user)
-    sender = Sender()
+    sender = SenderMock()
     spam_sender = SpamSender(session, sender)
     spam_sender.send_email(
         'paulob.bruno@gmail.com',
@@ -26,3 +38,21 @@ def test_qty_spam_sender(session, users):
         'Take a look into the lessons'
     )
     assert len(users) == sender.qty_sent_emails
+
+
+def test_spam_parameters(session):
+    user = User(name='Rafael', email='rafaelbuenobruno@gmail.com')
+    session.save(user)
+    sender = SenderMock()
+    spam_sender = SpamSender(session, sender)
+    spam_sender.send_email(
+        'paulob.bruno@gmail.com',
+        'Python pro Course',
+        'Take a look into the lessons'
+    )
+    assert sender.sending_parameters == (
+        'paulob.bruno@gmail.com',
+        'rafaelbuenobruno@gmail.com',
+        'Python pro Course',
+        'Take a look into the lessons'
+    )
